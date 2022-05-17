2540350fd6d3a4cdabb7dd0d162d7d4154c7c6f8
Vrajesh <vrajesh2badgujar@gmail.com>
2022-05-17 10:56:05 +0530

    parsing parametres to function practice ex

--- 08_07/script.js
+++ 08_07/script.js
@@ -3,17 +3,40 @@
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
  */
 
-const tipCalculator = () => {
-  let sum = 29.95;
-  let percentage = 18;
+// const tipCalculator = () => {
+//   let sum = 29.95;
+//   let percentage = 18;
+//   let tip = sum * (percentage / 100);
+//   let total = sum + tip;
+//   console.log(`
+//   Sum before tip: ${sum}
+//   Tip percentage: ${percentage}%
+//   Tip:            ${tip.toFixed(2)}
+//   Total:          ${total.toFixed(2)}
+// `);
+// };
+
+// tipCalculator();
+
+const tipCalculator = (sum, percentage, currency, prefix) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
-  console.log(`
-  Sum before tip: ${sum}
-  Tip percentage: ${percentage}%
-  Tip:            ${tip.toFixed(2)}
-  Total:          ${total.toFixed(2)}
-`);
+  if (prefix) {
+    console.log(`
+
+      sum before tip: ${currency}${sum}
+      tip percentage: ${percentage}%
+      tip:             ${currency}${tip.toFixed(2)}
+      total:          ${currency}${total.toFixed(2)}
+      `);
+  } else {
+    console.log(`
+  sum before tip:${sum} ${currency}
+  tip percentage: ${percentage}%
+  tip:             ${tip.toFixed(2)}${currency}
+  total:          ${total.toFixed(2)}${currency}  
+  `);
+  }
 };
 
-tipCalculator();
+tipCalculator(29.95, 28, "$", false);
